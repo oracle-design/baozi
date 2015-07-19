cc9176826702fc0feb565eb2beaa41c5d93db343
Vincent <vincent10e@gmail.com>
2015-07-20 01:02:29 +0800

    bind success

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -5,7 +5,9 @@ class ChatController < WebsocketRails::BaseController
   end
 
   def new_message
+    user_msg :new_message, message[:msg_body].dup
     puts ')'*40
+
   end
 
   def client_connected
@@ -14,5 +16,13 @@ class ChatController < WebsocketRails::BaseController
 
   def client_disconnected
     puts '&'*40
+  end
+
+  def user_msg(ev, msg)
+    broadcast_message ev, { 
+      # user_name:  connection_store[:user][:user_name], 
+      received:   Time.now.to_s(:short), 
+      msg_body:   ERB::Util.html_escape(msg) 
+      }
   end
 end
